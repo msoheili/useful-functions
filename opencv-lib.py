8513f5695d50206f92bc11b11c4e1da1cb495031
Soheili <msoheili@gmail.com>
2025-01-30 14:07:39 +0100

    First init

--- opencv-lib.py
+++ opencv-lib.py
@@ -1,1 +1,15 @@
+import numpy as np
+import cv2
+from PIL import Image
 
+def pil2cv(img: Image.Image) -> np.ndarray:
+    """Convert a PIL Image to an OpenCV (NumPy) array."""
+    img = np.array(img)  # Convert to NumPy array
+    
+    # Convert RGB to BGR (OpenCV default format)
+    if img.ndim == 3 and img.shape[2] == 3:  # RGB image
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    elif img.ndim == 3 and img.shape[2] == 4:  # RGBA image
+        img = cv2.cvtColor(img, cv2.COLOR_RGBA2BGRA)
+    
+    return img
